0edfe65bb25a8cdd78e8128fbfe203a1d1b830d4
Michael Morales <michael@jrayln.com>
2024-05-16 09:51:54 -0700

    Update layout.  Add helper function to detect `if (url_is($string)`

--- helpers/functions.php
+++ helpers/functions.php
@@ -6,3 +6,12 @@ if (!function_exists('get_flash_messages')) {
         return \JonathanRayln\Core\Application::$app->session->getFlash($key);
     }
 }
+
+if (!function_exists('url_is')) {
+    function url_is(string $url): bool
+    {
+        $url = '/' . trim(strtolower($url), '/');
+
+        return $url == \JonathanRayln\Core\Application::$app->request->getPath();
+    }
+}
